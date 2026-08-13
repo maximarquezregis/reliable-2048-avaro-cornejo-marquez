e059219d7e418f308689ba216b9be183e394c5de
jeremiasavaro <jeremiasavaro7@gmail.com>
2026-08-13 13:28:49 -0300

    Add setUp method before each test and simple test for move Left and Down.

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -2,9 +2,34 @@ package ar.edu.unrc.game2048;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static ar.edu.unrc.game2048.Board.*;
+
 class BoardTest {
 
-    @Test
-    void exampleTest() {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
     }
+
+    @Test
+    void moveDownChangeTheBoardTest() {
+        assertTrue(board.moveDown());
+    }
+
+    @Test
+    void moveLeftChangeTheBoardTest() {
+        assertTrue(board.moveLeft());
+    }
+
+    @Test
+    void getEmptyPositionsReturnsCorrectListTest() {
+        assertTrue(board.getEmptyPositions().size() > 0);
+    }
+
 }
